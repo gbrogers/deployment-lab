70b64ec46a62c6a62cfdace40dad8fc1cf2de8f1
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-07-15 12:27:09 -0600

    css test

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname));
 });
 
 const port = process.env.PORT || 4005;
