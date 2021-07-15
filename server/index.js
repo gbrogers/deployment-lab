30679e838267a36ecae21b24939057c7d6451c41
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-07-15 11:11:53 -0600

    inital commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+const port = process.env.PORT || 4005;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
